7be18a906659be512fe67053f46497da6bf6e471
Per <atlasfreak@gmx.de>
2023-12-21 05:42:50 +0100

    add javaFX application

---
+++ src/main/java/projekt/MyApplication.java
@@ -0,0 +1,26 @@
+package projekt;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import projekt.controller.SceneSwitcher;
+import projekt.controller.SceneSwitcher.SceneType;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setMinWidth(450);
+        stage.setMinHeight(400);
+
+        SceneSwitcher.loadScene(SceneType.MAIN_MENU, stage);
+    }
+
+    /**
+     * The main method of the application.
+     *
+     * @param args The launch arguments of the application.
+     */
+    public static void main(final String[] args) {
+        launch(args);
+    }
+}
