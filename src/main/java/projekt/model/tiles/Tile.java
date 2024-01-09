769305e5d85c4be5c8999cefe181f1aec4b510b9
Per <atlasfreak@gmx.de>
2024-01-09 02:21:21 +0100

    change Tile to record class remove Corner enum

--- src/main/java/projekt/model/tiles/Tile.java
+++ src/main/java/projekt/model/tiles/Tile.java
@@ -1,70 +1,24 @@
 package projekt.model.tiles;
 
-import projekt.model.Intersection;
 import projekt.model.Position;
 import projekt.model.ResourceType;
-
-import java.util.Map;
 
 import javafx.beans.value.ObservableDoubleValue;
 import javafx.scene.paint.Color;
 
-public interface Tile {
+public record Tile(Position position, Type type, int yield, ObservableDoubleValue heightProperty,
+        ObservableDoubleValue widthProperty) {
 
-    Position getPosition();
-
-    /**
-     * Returns the type of this tile.
-     *
-     * @see Type
-     * @return the type
-     */
-    Type getType();
-
-    /**
-     * Returns the resource that is available from this tile, if any.
-     *
-     * @return the resource
-     */
-    ResourceType getResource();
-
-    /**
-     * Returns the value at which this tile yields one of its resource.
-     *
-     * @return the value (determined by dice roll) required to yield this tile's
-     *         resource
-     */
-    int getYield();
-
-    /**
-     * Returns the width of this tile as an {@link ObservableDoubleValue}.
-     *
-     * @return the width of this tile as an {@link ObservableDoubleValue}
-     */
-    ObservableDoubleValue widthProperty();
-
-    /**
-     * Returns the height of this tile as an {@link ObservableDoubleValue}.
-     *
-     * @return the height of this tile as an {@link ObservableDoubleValue}
-     */
-    ObservableDoubleValue heightProperty();
-
-    enum Corner {
-        SOUTH,
-        SOUTH_WEST,
-        NORTH_WEST,
-        NORTH,
-        NORTH_EAST,
-        SOUTH_EAST
+    public Tile(int q, int r, Type type, int yield, ObservableDoubleValue heightProperty,
+            ObservableDoubleValue widthProperty) {
+        this(new Position(q, r), type, yield, heightProperty, widthProperty);
     }
 
     /**
      * An enumeration containing all available tile types.
-     * Custom tile types need to be added to this list manually. As well as a
-     * corresponding mapping to {@link Type#newTileInstance}.
+     * Custom tile types need to be added to this list manually.
      */
-    enum Type {
+    public enum Type {
         WOODLAND(Color.DARKGREEN, ResourceType.WOOD),
         MEADOW(Color.GREEN, ResourceType.CLAY),
         FARMLAND(Color.YELLOW, ResourceType.GRAIN),
