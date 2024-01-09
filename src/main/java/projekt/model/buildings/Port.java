7ca61db4feba338c4fb8ebd464ca43a819df8674
Per <atlasfreak@gmx.de>
2024-01-09 02:19:07 +0100

    cahnge port to record class

--- src/main/java/projekt/model/buildings/Port.java
+++ src/main/java/projekt/model/buildings/Port.java
@@ -1,41 +1,17 @@
 package projekt.model.buildings;
 
-import projekt.model.PlayerImpl;
+import projekt.model.Intersection;
 import projekt.model.ResourceType;
 
-public class Port {
-
-    private final int ratio;
-    private final ResourceType resourceType;
+public record Port(int ratio, Intersection intersection, ResourceType resourceType) {
 
     /**
-     * Constructs a new port with n:1 ratio, meaning that n resources of the same type can be traded for 1 resource of any type.
+     * Constructs a new port with n:1 ratio, meaning that n resources of the same
+     * type can be traded for 1 resource of any type.
+     *
      * @param ratio amount of same-type resources to trade
      */
-    public Port(int ratio) {
-        this(ratio, null);
-    }
-
-    /**
-     * Constructs a new port with n:1 ratio, meaning that n resources of the specified type can be traded for 1 resource of any type.
-     * @param ratio    amount of same-type resources to trade
-     * @param resourceType type of resource that
-     */
-    public Port(int ratio, ResourceType resourceType) {
-        this.ratio = ratio;
-        this.resourceType = resourceType;
-    }
-
-    public int getRatio() {
-        return ratio;
-    }
-
-    public ResourceType getResource() {
-        return resourceType;
-    }
-
-    // TODO: not sure this belongs here
-    public boolean canTrade(PlayerImpl player, ResourceType resourceType) {
-        return (this.resourceType == null || this.resourceType.equals(resourceType)) && player.getResourceAmount(resourceType) >= ratio;
+    public Port(int ratio, Intersection intersection) {
+        this(ratio, intersection, null);
     }
 }
