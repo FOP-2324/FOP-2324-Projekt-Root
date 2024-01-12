7a5c66a978ad77edb257d7902bd0bfc6a9c68b2a
Per Göttlicher <atlasfreak@gmx.de>
2024-01-12 17:36:02 +0100

    remove redundant public modifier

--- src/main/java/projekt/model/Player.java
+++ src/main/java/projekt/model/Player.java
@@ -14,7 +14,7 @@ public interface Player {
      *
      * @return a map of all resources the player currently has and how many of each.
      */
-    public Map<ResourceType, Integer> getResources();
+    Map<ResourceType, Integer> getResources();
 
     /**
      * Adds the given amount of the given resource to the player.
@@ -22,7 +22,7 @@ public interface Player {
      * @param resourceType the ResourceType to add to
      * @param amount       the amount to add
      */
-    public void addResource(ResourceType resourceType, int amount);
+    void addResource(ResourceType resourceType, int amount);
 
     /**
      * Removes the given amount of the given resource from the player.
@@ -30,91 +30,91 @@ public interface Player {
      * @param resourceType the ResourceType to remove from
      * @param amount       the amount to remove
      */
-    public void removeResource(ResourceType resourceType, int amount);
+    void removeResource(ResourceType resourceType, int amount);
 
     /**
      * Returns all roads the player currently has.
      *
      * @return all roads the player currently has
      */
-    public Set<Road> getRoads();
+    Set<Road> getRoads();
 
     /**
      * Adds the given road to the player.
      *
      * @param road the road to add
      */
-    public void addRoad(Road road);
+    void addRoad(Road road);
 
     /**
      * Returns the amount of roads the player can still build.
      *
      * @return the amount of roads the player can still build
      */
-    public int getRemainingRoads();
+    int getRemainingRoads();
 
     /**
      * Returns all settlements the player currently has.
      *
      * @return all settlements the player currently has
      */
-    public Set<Settlement> getSettlements();
+    Set<Settlement> getSettlements();
 
     /**
      * Adds the given settlement to the player.
      *
      * @param settlement the settlement to add
      */
-    public void addSettlement(Settlement settlement);
+    void addSettlement(Settlement settlement);
 
     /**
      * Removes the given settlement from the player.
      *
      * @param settlement the settlement to remove
      */
-    public void removeSettlement(Settlement settlement);
+    void removeSettlement(Settlement settlement);
 
     /**
      * Returns the amount of settlements the player can still build.
      *
      * @return the amount of settlements the player can still build
      */
-    public int getRemainingSettlements();
+    int getRemainingSettlements();
 
     /**
      * Returns a map of all development cards the player currently has and how many.
      *
      * @return a map of all development cards the player currently has and how many.
      */
-    public Map<DevelopmentCardType, Integer> getDevelopmentCards();
+    Map<DevelopmentCardType, Integer> getDevelopmentCards();
 
     /**
      * Adds the given development card to the player.
      *
      * @param developmentCardType the development card to add
      */
-    public void addDevelopmentCard(DevelopmentCardType developmentCardType);
+    void addDevelopmentCard(DevelopmentCardType developmentCardType);
 
     /**
      * Removes the given development card from the player.
      *
      * @param developmentCardType the development card to remove
      */
-    public void removeDevelopmentCard(DevelopmentCardType developmentCardType);
+    void removeDevelopmentCard(DevelopmentCardType developmentCardType);
 
     /**
      * Returns the total amount of development cards the player has.
      *
      * @return the total amount of development cards the player has
      */
-    public int getTotalDevelopmentCards();
+    int getTotalDevelopmentCards();
 
     /**
      * Returns the amount of knights the player has played.
      *
      * @return the amount of knights the player has played
      */
-    public int getKnightsPlayed();
+    int getKnightsPlayed();
 
     /**
      * Returns the color of the player.
