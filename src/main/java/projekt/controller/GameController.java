b027dd2dd04314f65b3908d893a6ed1c819d1847
Rdeisenroth <rdeisenroth@gmail.com>
2024-01-14 20:31:01 +0100

    fix GameController

--- src/main/java/projekt/controller/GameController.java
+++ src/main/java/projekt/controller/GameController.java
@@ -1,47 +1,45 @@
 package projekt.controller;
 
+import javafx.scene.paint.Color;
 import projekt.Config;
 import projekt.model.HexGrid;
-import projekt.model.Intersection;
+import projekt.model.HexGridImpl;
 import projekt.model.Player;
-import projekt.model.Position;
-import projekt.model.buildings.Settlement;
+import projekt.model.PlayerImpl;
 import projekt.model.tiles.Tile;
 import projekt.view.GameBoardBuilder;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class GameController extends SceneController {
 
     private static GameController INSTANCE;
-    private final HexGrid gameBoard;
+    private final HexGrid grid;
     private final List<Player> players = new ArrayList<>();
     private Tile banditTile = null;
 
-    private GameController(HexGrid gameBoard) {
-        super(new GameBoardBuilder(HexGridController.getHexGridController(gameBoard).getView()));
-        this.gameBoard = gameBoard;
+    private GameController(final HexGrid grid) {
+        super(new GameBoardBuilder(HexGridController.getHexGridController(grid).getView()));
+        this.grid = grid;
     }
 
     public static GameController getInstance() {
         if (INSTANCE == null) {
-            INSTANCE = new GameController(new HexGrid());
+            INSTANCE = new GameController(new HexGridImpl(Config.GRID_RADIUS));
         }
 
         return INSTANCE;
     }
 
-    public HexGrid getGameBoard() {
-        return gameBoard;
+    public HexGrid getGrid() {
+        return grid;
     }
 
     public Player newPlayer() {
-        Player player = new Player();
+        final Player player = new PlayerImpl(grid, Color.AQUA);
         players.add(player);
         return player;
     }
@@ -54,45 +52,45 @@ public class GameController extends SceneController {
         return banditTile;
     }
 
-    public void setBanditTile(Tile tile) {
+    public void setBanditTile(final Tile tile) {
         this.banditTile = tile;
     }
 
     public int castDice() {
         return IntStream.rangeClosed(1, Config.NUMBER_OF_DICE)
-                .map(i -> Config.RANDOM.nextInt(1, Config.DICE_SIDES + 1))
-                .sum();
+            .map(i -> Config.RANDOM.nextInt(1, Config.DICE_SIDES + 1))
+            .sum();
     }
 
     public void resourcePhase() {
-        int diceValue = castDice();
+        final int diceValue = castDice();
 
-        if (diceValue != 7) {
-            Set<? extends Settlement> settlements = players.stream()
-                    .flatMap(player -> player.getStructures()
-                            .stream()
-                            .filter(structure -> structure instanceof Settlement)
-                            .map(structure -> (Settlement) structure))
-                    .collect(Collectors.toSet());
-
-            gameBoard.getTiles()
-                    .stream()
-                    .filter(tile -> tile.getYield() == diceValue)
-                    .forEach(tile -> {
-                        Set<Position> adjacentIntersections = tile.getAdjacentIntersections()
-                                .values()
-                                .stream()
-                                .map(Intersection::getPosition)
-                                .collect(Collectors.toSet());
-                        settlements.forEach(settlement -> {
-                            if (adjacentIntersections.contains(settlement.getPosition())) {
-                                settlement.getOwner().addResource(tile.getResource(), 1);
-                            }
-                        });
-                    });
-        } else {
-            // bandit becomes active
-        }
+//        if (diceValue != 7) {
+//            final Set<? extends Settlement> settlements = players.stream()
+//                .flatMap(player -> player.getStructures()
+//                    .stream()
+//                    .filter(structure -> structure instanceof Settlement)
+//                    .map(structure -> (Settlement) structure))
+//                .collect(Collectors.toSet());
+//
+//            grid.getTiles()
+//                .stream()
+//                .filter(tile -> tile.getYield() == diceValue)
+//                .forEach(tile -> {
+//                    final Set<TilePosition> adjacentIntersections = tile.getAdjacentIntersections()
+//                        .values()
+//                        .stream()
+//                        .map(Intersection::getPosition)
+//                        .collect(Collectors.toSet());
+//                    settlements.forEach(settlement -> {
+//                        if (adjacentIntersections.contains(settlement.getPosition())) {
+//                            settlement.getOwner().addResource(tile.getResource(), 1);
+//                        }
+//                    });
+//                });
+//        } else {
+//            // bandit becomes active
+//        }
     }
 
     @Override
