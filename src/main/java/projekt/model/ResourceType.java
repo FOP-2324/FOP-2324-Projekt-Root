105def512f05a32b5d8b96f98d9bcedda3c7e902
Per <atlasfreak@gmx.de>
2024-01-16 03:31:15 +0100

    Add color to ResourceType

--- src/main/java/projekt/model/ResourceType.java
+++ src/main/java/projekt/model/ResourceType.java
@@ -1,9 +1,17 @@
 package projekt.model;
 
+import javafx.scene.paint.Color;
+
 public enum ResourceType {
-    WOOD,
-    CLAY,
-    WOOL,
-    GRAIN,
-    ORE
+    WOOD(Color.DARKGREEN),
+    CLAY(Color.SIENNA),
+    WOOL(Color.LINEN),
+    GRAIN(Color.YELLOW),
+    ORE(Color.SLATEGRAY);
+
+    public final Color color;
+
+    ResourceType(Color color) {
+        this.color = color;
+    }
 }
