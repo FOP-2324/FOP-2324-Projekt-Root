d891e42c644366ec39a2f2633a97acae2a8b3bf8
Per Göttlicher <atlasfreak@gmx.de>
2024-01-19 18:50:02 +0100

    add methods to highlight a intersection and make it clickable

--- src/main/java/projekt/controller/gui/IntersectionController.java
+++ src/main/java/projekt/controller/gui/IntersectionController.java
@@ -1,20 +1,34 @@
 package projekt.controller.gui;
 
+import java.util.function.Consumer;
+
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Region;
 import javafx.util.Builder;
 import projekt.model.Intersection;
 import projekt.view.IntersectionBuilder;
 
 public class IntersectionController implements Controller {
-    private final Builder<Region> buidler;
+    private final IntersectionBuilder buidler;
 
     public IntersectionController(Intersection intersection) {
         this.buidler = new IntersectionBuilder(intersection);
+    }
+
+    public Intersection getIntersection() {
+        return buidler.getIntersection();
+    }
+
+    public void highlight(Consumer<MouseEvent> handler) {
+        buidler.highlight(handler);
+    }
+
+    public void unhighlight() {
+        buidler.unhighlight();
     }
 
     @Override
     public Builder<Region> getBuilder() {
         return buidler;
     }
-
 }
--- src/main/java/projekt/view/IntersectionBuilder.java
+++ src/main/java/projekt/view/IntersectionBuilder.java
@@ -1,14 +1,20 @@
 package projekt.view;
 
+import java.util.function.Consumer;
+
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.util.Builder;
 import projekt.model.Intersection;
 import projekt.model.buildings.Settlement;
 
 public class IntersectionBuilder implements Builder<Region> {
     private final Intersection intersection;
+    private final StackPane pane = new StackPane();
 
     public IntersectionBuilder(Intersection intersection) {
         this.intersection = intersection;
@@ -16,13 +22,13 @@ public class IntersectionBuilder implements Builder<Region> {
 
     @Override
     public Region build() {
-        StackPane pane = new StackPane();
         ImageView settlement = getSettlement();
         // Circle circle = new Circle(10, Color.RED);
         // pane.getChildren().add(circle);
         if (settlement != null) {
             pane.getChildren().add(settlement);
         }
+        unhighlight();
         pane.getStyleClass().add("intersection");
         return pane;
     }
@@ -41,4 +47,19 @@ public class IntersectionBuilder implements Builder<Region> {
         return settlement;
     }
 
+    public Intersection getIntersection() {
+        return intersection;
+    }
+
+    public void highlight(Consumer<MouseEvent> handler) {
+        System.out.println("Highlighting intersection");
+        Circle circle = new Circle(10, Color.RED);
+        pane.getChildren().add(circle);
+        pane.setOnMouseClicked(handler::accept);
+    }
+
+    public void unhighlight() {
+        pane.getChildren().removeIf(node -> node instanceof Circle);
+        pane.setOnMouseClicked(null);
+    }
 }
