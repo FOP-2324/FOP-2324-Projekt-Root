3026ef53b869c03f514d3562b9899477455d33d1
Per <atlasfreak@gmx.de>
2024-01-24 18:31:48 +0100

    adjust GameBoardController for new gameController and add PlayerActions make Player overview and information updatable

--- src/main/java/projekt/controller/gui/GameBoardController.java
+++ src/main/java/projekt/controller/gui/GameBoardController.java
@@ -3,26 +3,53 @@ package projekt.controller.gui;
 import javafx.scene.layout.Region;
 import javafx.util.Builder;
 import projekt.controller.GameController;
+import projekt.model.Player;
 import projekt.view.GameBoardBuilder;
 
 public class GameBoardController implements SceneController {
     private final GameController gameController;
-    private final Builder<Region> gameBoardBuilder;
+    private final PlayerActionsController playerActionsController;
+    private final HexGridController hexGridController;
 
-    protected GameBoardController(final GameController gameController) {
-        this.gameBoardBuilder = new GameBoardBuilder(new HexGridController(gameController.getGrid()).buildView(),
-                gameController.getPlayers());
+    private final GameBoardBuilder gameBoardBuilder;
+
+    public GameBoardController(final GameController gameController) {
+        this.playerActionsController = new PlayerActionsController(this,
+                gameController.getActivePlayerControllerProperty());
+        this.hexGridController = new HexGridController(gameController.getState().getGrid());
+        this.gameBoardBuilder = new GameBoardBuilder(hexGridController.buildView(), playerActionsController::buildView);
         this.gameController = gameController;
+        gameController.getActivePlayerControllerProperty().addListener((observable, oldValue, newValue) -> {
+            updatePlayerInformation(newValue.getPlayer());
+        });
+    }
+
+    public HexGridController getHexGridController() {
+        return hexGridController;
+    }
+
+    public GameController getGameController() {
+        return gameController;
+    }
+
+    public void updatePlayerInformation(Player player) {
+        gameBoardBuilder.updatePlayerInformation(player, gameController.getState().getPlayers());
     }
 
     @Override
     public String getTitle() {
-        return "Catan Runde 1";
+        return "Catan";
     }
 
     @Override
     public Builder<Region> getBuilder() {
         return gameBoardBuilder;
+    }
+
+    @Override
+    public Region buildView() {
+        gameController.startGame();
+        return gameBoardBuilder.build();
     }
 
 }
--- src/main/java/projekt/view/GameBoardBuilder.java
+++ src/main/java/projekt/view/GameBoardBuilder.java
@@ -1,13 +1,17 @@
 package projekt.view;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 import javafx.beans.binding.Bindings;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
@@ -19,11 +23,12 @@ import projekt.view.gameControls.PlayersOverviewBuilder;
 
 public class GameBoardBuilder implements Builder<Region> {
     private final Region hexGrid;
-    private final List<Player> players;
+    private final Supplier<Region> actions;
+    private final Pane playerInformation = new VBox();
 
-    public GameBoardBuilder(final Region hexGrid, final List<Player> players) {
+    public GameBoardBuilder(final Region hexGrid, final Supplier<Region> actions) {
         this.hexGrid = hexGrid;
-        this.players = players;
+        this.actions = actions;
     }
 
     @Override
@@ -31,23 +36,37 @@ public class GameBoardBuilder implements Builder<Region> {
         final GridPane mainPane = new GridPane();
         mainPane.add(hexGrid, 0, 0);
 
-        VBox playersInformationBox = new VBox();
-        playersInformationBox.getChildren().add(new PlayerInformationBuilder(players.get(0)).build());
-        playersInformationBox.getChildren().add(new PlayersOverviewBuilder(players).build());
-        playersInformationBox.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
-        ScrollPane playersInformationPane = new ScrollPane(playersInformationBox);
-        playersInformationBox.setMinWidth(150);
-        playersInformationBox.maxWidthProperty().bind(Bindings
+        // Right box which holds the Players information
+
+        VBox rightBox = new VBox();
+        Label diceRoll = new Label(String.format("Rolled Number: %d", 0));
+        rightBox.getChildren().add(diceRoll);
+        rightBox.getChildren().add(playerInformation);
+
+        rightBox.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+
+        ScrollPane playersInformationPane = new ScrollPane(rightBox);
+        rightBox.setMinWidth(150);
+        rightBox.maxWidthProperty().bind(Bindings
                 .createDoubleBinding(() -> playersInformationPane.getWidth() - 2,
                         playersInformationPane.widthProperty()));
 
+        // Bottom box which holds the Player controls
+
+        HBox bottomBox = new HBox();
+        bottomBox.getChildren().add(actions.get());
+        bottomBox.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+
+        mainPane.add(bottomBox, 0, 1);
         mainPane.add(playersInformationPane, 1, 0, 1, 2);
         mainPane.setGridLinesVisible(true);
 
+        // Make it look pretty
+
         ColumnConstraints hexGridColumn = new ColumnConstraints();
-        hexGridColumn.setPercentWidth(85);
+        hexGridColumn.setPercentWidth(75);
         ColumnConstraints playersInformationColumn = new ColumnConstraints();
-        playersInformationColumn.setPercentWidth(15);
+        playersInformationColumn.setPercentWidth(25);
 
         RowConstraints hexGridRow = new RowConstraints();
         hexGridRow.setPercentHeight(90);
@@ -65,4 +84,9 @@ public class GameBoardBuilder implements Builder<Region> {
         return mainPane;
     }
 
+    public void updatePlayerInformation(Player player, List<Player> players) {
+        playerInformation.getChildren().clear();
+        playerInformation.getChildren().add(new PlayerInformationBuilder(player).build());
+        playerInformation.getChildren().add(new PlayersOverviewBuilder(players).build());
+    }
 }
