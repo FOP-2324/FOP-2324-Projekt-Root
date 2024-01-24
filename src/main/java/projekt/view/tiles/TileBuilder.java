baf4d448eb09dbdac9f6132fbb76199b385359e6
Per <atlasfreak@gmx.de>
2024-01-24 18:46:04 +0100

    add robber to the tile

--- src/main/java/projekt/view/tiles/TileBuilder.java
+++ src/main/java/projekt/view/tiles/TileBuilder.java
@@ -1,14 +1,19 @@
 package projekt.view.tiles;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.util.Builder;
 import projekt.model.tiles.Tile;
+import projekt.view.ColoredImageView;
 
 public class TileBuilder implements Builder<Region> {
     private final Tile tile;
@@ -21,6 +26,12 @@ public class TileBuilder implements Builder<Region> {
     public Region build() {
         final StackPane pane = new StackPane();
         styleAndSizeTile(pane);
+        if (tile.hasRobber()) {
+            ImageView robber = new ColoredImageView("img/knight.png", Color.BLACK);
+            robber.setPreserveRatio(true);
+            robber.setFitWidth(tile.widthProperty().get() * 0.3);
+            pane.getChildren().add(robber);
+        }
         pane.getChildren().addAll(createCoordinateLabel());
         return pane;
     }
@@ -44,6 +55,8 @@ public class TileBuilder implements Builder<Region> {
         rollNumberLabel.getStyleClass().add("coordinate-label");
         detailsBox.getChildren().addAll(positionLabel, resourceLabel, rollNumberLabel);
         detailsBox.setAlignment(Pos.CENTER);
+        rollNumberLabel.setBackground(new Background(
+                new BackgroundFill(new Color(1, 1, 1, 0.9), new CornerRadii(2), new Insets(0, -2, 0, -2))));
         return detailsBox;
     }
 }
