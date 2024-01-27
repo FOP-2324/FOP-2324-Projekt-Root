68944ba89fa1fdea170106a2c9ff4d08f56c4209
Per <atlasfreak@gmx.de>
2024-01-27 02:08:03 +0100

    add Actions to objectives

--- src/main/java/projekt/controller/PlayerObjective.java
+++ src/main/java/projekt/controller/PlayerObjective.java
@@ -1,18 +1,22 @@
 package projekt.controller;
 
-import projekt.controller.actions.BuildVillageAction;
-import projekt.controller.actions.PlayerAction;
-import projekt.controller.actions.RollDiceAction;
-
 import java.util.Set;
 
+import projekt.controller.actions.BuildRoadAction;
+import projekt.controller.actions.BuildVillageAction;
+import projekt.controller.actions.EndTurnAction;
+import projekt.controller.actions.PlayerAction;
+import projekt.controller.actions.RollDiceAction;
+import projekt.controller.actions.UpgradeVillageAction;
+
 public enum PlayerObjective {
-    DROP_HALF_CARDS(Set.of()),
-    SELECT_CARD_TO_STEAL(Set.of()),
-    SELECT_ROBBER_TILE(Set.of()),
-    REGULAR_TURN(Set.of(BuildVillageAction.class)),
-    PLACE_TWO_VILLAGES(Set.of()),
-    PLACE_TWO_ROADS(Set.of()),
+    DROP_HALF_CARDS(Set.of(EndTurnAction.class)),
+    SELECT_CARD_TO_STEAL(Set.of(EndTurnAction.class)),
+    SELECT_ROBBER_TILE(Set.of(EndTurnAction.class)),
+    REGULAR_TURN(
+            Set.of(BuildRoadAction.class, EndTurnAction.class, BuildVillageAction.class, UpgradeVillageAction.class)),
+    PLACE_VILLAGE(Set.of(BuildVillageAction.class)),
+    PLACE_ROAD(Set.of(BuildRoadAction.class)),
     DICE_ROLL(Set.of(RollDiceAction.class)),
     IDLE(Set.of());
 
