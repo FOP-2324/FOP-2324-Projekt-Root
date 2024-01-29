5bdaa339e2258cdbc47e3b61b502db9cda8e1770
Per <atlasfreak@gmx.de>
2024-01-29 03:15:18 +0100

    add and use PlayerLabel

---
+++ src/main/java/projekt/view/PlayerLabel.java
@@ -0,0 +1,19 @@
+package projekt.view;
+
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import projekt.model.Player;
+
+/**
+ * A Label that displays a player's name and color.
+ */
+public class PlayerLabel extends Label {
+    public PlayerLabel(final Player player) {
+        super(player.getName());
+        final Rectangle playerColor = new Rectangle(20, 20, player.getColor());
+        playerColor.setStroke(Color.BLACK);
+        playerColor.setStrokeWidth(2);
+        setGraphic(playerColor);
+    }
+}
--- src/main/java/projekt/view/gameControls/PlayerInformationBuilder.java
+++ src/main/java/projekt/view/gameControls/PlayerInformationBuilder.java
@@ -8,62 +8,58 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.util.Builder;
 import projekt.model.DevelopmentCardType;
 import projekt.model.Player;
 import projekt.model.ResourceType;
 import projekt.view.CardPane;
+import projekt.view.PlayerLabel;
 import projekt.view.ResourceCardPane;
 import projekt.view.Utils;
 
 public class PlayerInformationBuilder implements Builder<Region> {
     private final Player player;
 
-    public PlayerInformationBuilder(Player player) {
+    public PlayerInformationBuilder(final Player player) {
         this.player = player;
     }
 
     @Override
     @StudentImplementationRequired
     public Region build() {
-        VBox mainBox = new VBox();
+        final VBox mainBox = new VBox();
         // TODO: get player name when correctly implemented...
-        Label playerName = new Label("Placeholder for name");
+        final Label playerName = new PlayerLabel(player);
 
-        Rectangle playerColor = new Rectangle(20, 20, player.getColor());
-        playerColor.setStroke(Color.BLACK);
-        playerColor.setStrokeWidth(2);
-        playerName.setGraphic(playerColor);
-
-        Label resourcesLabel = new Label("Deine Resourcen:");
-        FlowPane resourcesBox = new FlowPane(5, 5);
-        for (ResourceType resourceType : player.getResources().keySet()) {
+        final Label resourcesLabel = new Label("Deine Resourcen:");
+        final FlowPane resourcesBox = new FlowPane(5, 5);
+        for (final ResourceType resourceType : player.getResources().keySet()) {
             if (player.getResources().get(resourceType) == 0) {
                 continue;
             }
 
-            ResourceCardPane resourceCard = new ResourceCardPane(resourceType, player.getResources().get(resourceType));
+            final ResourceCardPane resourceCard = new ResourceCardPane(resourceType,
+                    player.getResources().get(resourceType));
             resourcesBox.getChildren().add(resourceCard);
         }
 
-        Label developmentCardsLabel = new Label("Deine Entwicklungskarten:");
-        FlowPane developmentCardsBox = new FlowPane(5, 5);
-        for (DevelopmentCardType developmentCardType : player.getDevelopmentCards().keySet()) {
-            CardPane developmentCardTypeCard = new CardPane(Color.LIGHTGRAY,
+        final Label developmentCardsLabel = new Label("Deine Entwicklungskarten:");
+        final FlowPane developmentCardsBox = new FlowPane(5, 5);
+        for (final DevelopmentCardType developmentCardType : player.getDevelopmentCards().keySet()) {
+            final CardPane developmentCardTypeCard = new CardPane(Color.LIGHTGRAY,
                     Integer.toString(player.getDevelopmentCards().get(developmentCardType)));
             Utils.attachTooltip(developmentCardType.toString(), developmentCardTypeCard);
             developmentCardsBox.getChildren().add(developmentCardTypeCard);
         }
 
-        Label remainingRoadsLabel = new Label(
+        final Label remainingRoadsLabel = new Label(
                 String.format("Deine verbleibenden Straßen: %d", player.getRemainingRoads()));
-        Label remainingVillagesLabel = new Label(
+        final Label remainingVillagesLabel = new Label(
                 String.format("Deine verbleibenden Dörfer: %d", player.getRemainingVillages()));
-        Label remainingCitiesLabel = new Label(
+        final Label remainingCitiesLabel = new Label(
                 String.format("Deine verbleibenden Städte: %d", player.getRemainingCities()));
 
-        Label victoryPointsLabel = new Label(String.format("Deine Siegpunkte: %d", player.getVictoryPoints()));
+        final Label victoryPointsLabel = new Label(String.format("Deine Siegpunkte: %d", player.getVictoryPoints()));
 
         mainBox.getChildren().addAll(playerName, resourcesLabel, resourcesBox, developmentCardsLabel,
                 developmentCardsBox, remainingRoadsLabel, remainingVillagesLabel, remainingCitiesLabel,
