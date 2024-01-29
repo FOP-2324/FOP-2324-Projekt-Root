3c1b41070ec5dd0fd73546155fff960ee9e3219f
Per <atlasfreak@gmx.de>
2024-01-29 23:03:32 +0100

    add documentatio for settlement types

--- src/main/java/projekt/model/buildings/Settlement.java
+++ src/main/java/projekt/model/buildings/Settlement.java
@@ -4,6 +4,15 @@ import projekt.model.Intersection;
 import projekt.model.Player;
 
 public record Settlement(Player owner, Type type, Intersection intersection) {
+
+    /**
+     * Defines the different types of settlements.
+     *
+     * The icons are determined by the order of the enum values.
+     * first value: village
+     * second value: city
+     * potential third value: metropolis //TODO: provide graphics for metropolis
+     */
     public enum Type {
         VILLAGE(1),
         CITY(2);
