d449b4b1f9ae9186eb32d8219c5eb122352fdb44
Per <atlasfreak@gmx.de>
2024-01-31 03:19:11 +0100

    add documentation and DoNotTouch annotation

--- src/main/java/projekt/controller/gui/Controller.java
+++ src/main/java/projekt/controller/gui/Controller.java
@@ -1,11 +1,27 @@
 package projekt.controller.gui;
 
+import org.tudalgo.algoutils.student.annotation.DoNotTouch;
+
 import javafx.scene.layout.Region;
 import javafx.util.Builder;
 
+/**
+ * A controller for a view.
+ */
+@DoNotTouch
 public interface Controller {
+    /**
+     * Returns a builder for the view of this controller.
+     *
+     * @return a builder for the view of this controller
+     */
     Builder<Region> getBuilder();
 
+    /**
+     * Returns the view of this controller.
+     *
+     * @return the view of this controller
+     */
     default Region buildView() {
         return getBuilder().build();
     }
