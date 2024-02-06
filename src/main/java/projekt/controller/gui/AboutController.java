ab183e86ed5c6d5b17436221658439c654f32219
Per <atlasfreak@gmx.de>
2024-02-06 20:23:30 +0100

    add documentation to gui controllers

--- src/main/java/projekt/controller/gui/AboutController.java
+++ src/main/java/projekt/controller/gui/AboutController.java
@@ -4,8 +4,10 @@ import javafx.scene.layout.Region;
 import javafx.util.Builder;
 import projekt.view.menus.AboutBuilder;
 
+/**
+ * The controller for the about scene.
+ */
 public class AboutController implements SceneController {
-
     @Override
     public Builder<Region> getBuilder() {
         return new AboutBuilder(SceneController::loadMainMenuScene);
@@ -15,5 +17,4 @@ public class AboutController implements SceneController {
     public String getTitle() {
         return "About FOP-Project WiSe 23/24";
     }
-
 }
