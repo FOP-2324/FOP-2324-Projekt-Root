2841dc53956b86bab227299c738867b7dcfc6ae1
Per Göttlicher <atlasfreak@gmx.de>
2024-02-09 13:39:17 +0100

    change Development Card Type documentation

--- src/main/java/projekt/model/DevelopmentCardType.java
+++ src/main/java/projekt/model/DevelopmentCardType.java
@@ -2,6 +2,14 @@ package projekt.model;
 
 /**
  * The different types of development cards.
+ * Each development card has an icon index associated with it.
+ * Icon index is used to determine which icon to use for the development card.
+ * The different icons are:
+ * 0: Road Building
+ * 1: Laurels for victory points
+ * 2: Light Bulb for invention
+ * 3: Knight
+ * 4: Monopoly
  */
 public enum DevelopmentCardType {
     KNIGHT(3),
