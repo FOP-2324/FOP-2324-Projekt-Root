e05b26fead536e3256308fd8e41f661deedc9651
Daniel Mangold <mangold.daniel@protonmail.com>
2024-04-15 11:03:28 +0200

    WIP H3 - Add tests for updateUpgradeVillageButtonState and updateBuildRoadButtonState

--- src/graderPrivate/java/projekt/controller/gui/PlayerActionsControllerTest.java
+++ src/graderPrivate/java/projekt/controller/gui/PlayerActionsControllerTest.java
@@ -16,6 +16,7 @@ import projekt.controller.GameController;
 import projekt.controller.PlayerController;
 import projekt.controller.PlayerObjective;
 import projekt.model.*;
+import projekt.model.buildings.Edge;
 import projekt.util.Utils;
 import projekt.view.gameControls.PlayerActionsBuilder;
 
@@ -165,6 +166,38 @@ public class PlayerActionsControllerTest {
         setup(updateBuildVillageButtonStateMethod.getName(),
                 calledEnableBuildVillageButton,
                 calledDisableBuildVillageButton,
+                PlayerObjective.REGULAR_TURN,
+                new PlayerState(
+                        new AbstractSet<>() {
+                            @Override
+                            public Iterator<Intersection> iterator() {
+                                return Iterators.singletonIterator(null);
+                            }
+
+                            @Override
+                            public int size() {
+                                return 1;
+                            }
+                        },
+                        Collections.emptySet(),
+                        Collections.emptySet(),
+                        Collections.emptyList(),
+                        null,
+                        0,
+                        Collections.emptyMap()));
+        call(() -> updateBuildVillageButtonStateMethod.invoke(playerActionsController), baseContext, result ->
+                "PlayerActionsController.updateBuildVillageButtonState threw an uncaught exception");
+        assertTrue(calledEnableBuildVillageButton.get(), baseContext, result ->
+                "PlayerActionsController.updateBuildVillageButtonState did not call builder.enableBuildVillageButton");
+        assertFalse(calledDisableBuildVillageButton.get(), baseContext, result ->
+                "PlayerActionsController.updateBuildVillageButtonState called builder.disableBuildVillageButton");
+
+        // correct objective, one buildable intersection
+        calledEnableBuildVillageButton.set(false);
+        calledDisableBuildVillageButton.set(false);
+        setup(updateBuildVillageButtonStateMethod.getName(),
+                calledEnableBuildVillageButton,
+                calledDisableBuildVillageButton,
                 PlayerObjective.PLACE_VILLAGE,
                 new PlayerState(
                         new AbstractSet<>() {
@@ -190,5 +223,225 @@ public class PlayerActionsControllerTest {
                 "PlayerActionsController.updateBuildVillageButtonState did not call builder.enableBuildVillageButton");
         assertFalse(calledDisableBuildVillageButton.get(), baseContext, result ->
                 "PlayerActionsController.updateBuildVillageButtonState called builder.disableBuildVillageButton");
+    }
+
+    @Test
+    public void testUpdateUpgradeVillageButtonState() throws ReflectiveOperationException {
+        Method updateUpgradeVillageButtonStateMethod = PlayerActionsController.class.getDeclaredMethod("updateUpgradeVillageButtonState");
+        updateUpgradeVillageButtonStateMethod.trySetAccessible();
+
+        // wrong objective, no buildable intersections
+        AtomicBoolean calledEnableUpgradeVillageButton = new AtomicBoolean();
+        AtomicBoolean calledDisableUpgradeVillageButton = new AtomicBoolean();
+        setup(updateUpgradeVillageButtonStateMethod.getName(),
+            calledEnableUpgradeVillageButton,
+            calledDisableUpgradeVillageButton,
+            PlayerObjective.IDLE,
+            new PlayerState(
+                Collections.emptySet(),
+                Collections.emptySet(),
+                Collections.emptySet(),
+                Collections.emptyList(),
+                null,
+                0,
+                Collections.emptyMap()));
+
+        executionHandler.disableMethodDelegation(updateUpgradeVillageButtonStateMethod);
+        call(() -> updateUpgradeVillageButtonStateMethod.invoke(playerActionsController), baseContext, result ->
+            "PlayerActionsController.updateUpgradeVillageButtonState threw an uncaught exception");
+        assertFalse(calledEnableUpgradeVillageButton.get(), baseContext, result ->
+            "PlayerActionsController.updateUpgradeVillageButtonState called builder.enableUpgradeVillageButton");
+        assertTrue(calledDisableUpgradeVillageButton.get(), baseContext, result ->
+            "PlayerActionsController.updateUpgradeVillageButtonState did not call builder.disableUpgradeVillageButton");
+
+        // wrong objective, one buildable intersection
+        calledEnableUpgradeVillageButton.set(false);
+        calledDisableUpgradeVillageButton.set(false);
+        setup(updateUpgradeVillageButtonStateMethod.getName(),
+            calledEnableUpgradeVillageButton,
+            calledDisableUpgradeVillageButton,
+            PlayerObjective.IDLE,
+            new PlayerState(
+                Collections.emptySet(),
+                new AbstractSet<>() {
+                    @Override
+                    public Iterator<Intersection> iterator() {
+                        return Iterators.singletonIterator(null);
+                    }
+
+                    @Override
+                    public int size() {
+                        return 1;
+                    }
+                },
+                Collections.emptySet(),
+                Collections.emptyList(),
+                null,
+                0,
+                Collections.emptyMap()));
+        call(() -> updateUpgradeVillageButtonStateMethod.invoke(playerActionsController), baseContext, result ->
+            "PlayerActionsController.updateUpgradeVillageButtonState threw an uncaught exception");
+        assertFalse(calledEnableUpgradeVillageButton.get(), baseContext, result ->
+            "PlayerActionsController.updateUpgradeVillageButtonState called builder.enableUpgradeVillageButton");
+        assertTrue(calledDisableUpgradeVillageButton.get(), baseContext, result ->
+            "PlayerActionsController.updateUpgradeVillageButtonState did not call builder.disableUpgradeVillageButton");
+
+        // correct objective, one buildable intersection
+        calledEnableUpgradeVillageButton.set(false);
+        calledDisableUpgradeVillageButton.set(false);
+        setup(updateUpgradeVillageButtonStateMethod.getName(),
+            calledEnableUpgradeVillageButton,
+            calledDisableUpgradeVillageButton,
+            PlayerObjective.REGULAR_TURN,
+            new PlayerState(
+                Collections.emptySet(),
+                new AbstractSet<>() {
+                    @Override
+                    public Iterator<Intersection> iterator() {
+                        return Iterators.singletonIterator(null);
+                    }
+
+                    @Override
+                    public int size() {
+                        return 1;
+                    }
+                },
+                Collections.emptySet(),
+                Collections.emptyList(),
+                null,
+                0,
+                Collections.emptyMap()));
+        call(() -> updateUpgradeVillageButtonStateMethod.invoke(playerActionsController), baseContext, result ->
+            "PlayerActionsController.updateUpgradeVillageButtonState threw an uncaught exception");
+        assertTrue(calledEnableUpgradeVillageButton.get(), baseContext, result ->
+            "PlayerActionsController.updateUpgradeVillageButtonState did not call builder.enableUpgradeVillageButton");
+        assertFalse(calledDisableUpgradeVillageButton.get(), baseContext, result ->
+            "PlayerActionsController.updateUpgradeVillageButtonState called builder.disableUpgradeVillageButton");
+    }
+
+    @Test
+    public void testUpdateBuildRoadButtonState() throws ReflectiveOperationException {
+        Method updateBuildRoadButtonStateMethod = PlayerActionsController.class.getDeclaredMethod("updateBuildRoadButtonState");
+        updateBuildRoadButtonStateMethod.trySetAccessible();
+
+        // wrong objective, no buildable intersections
+        AtomicBoolean calledEnableBuildRoadButton = new AtomicBoolean();
+        AtomicBoolean calledDisableBuildRoadButton = new AtomicBoolean();
+        setup(updateBuildRoadButtonStateMethod.getName(),
+            calledEnableBuildRoadButton,
+            calledDisableBuildRoadButton,
+            PlayerObjective.IDLE,
+            new PlayerState(
+                Collections.emptySet(),
+                Collections.emptySet(),
+                Collections.emptySet(),
+                Collections.emptyList(),
+                null,
+                0,
+                Collections.emptyMap()));
+
+        executionHandler.disableMethodDelegation(updateBuildRoadButtonStateMethod);
+        call(() -> updateBuildRoadButtonStateMethod.invoke(playerActionsController), baseContext, result ->
+            "PlayerActionsController.updateBuildRoadButtonState threw an uncaught exception");
+        assertFalse(calledEnableBuildRoadButton.get(), baseContext, result ->
+            "PlayerActionsController.updateBuildRoadButtonState called builder.enableBuildRoadButton");
+        assertTrue(calledDisableBuildRoadButton.get(), baseContext, result ->
+            "PlayerActionsController.updateBuildRoadButtonState did not call builder.disableBuildRoadButton");
+
+        // wrong objective, one buildable intersection
+        calledEnableBuildRoadButton.set(false);
+        calledDisableBuildRoadButton.set(false);
+        setup(updateBuildRoadButtonStateMethod.getName(),
+            calledEnableBuildRoadButton,
+            calledDisableBuildRoadButton,
+            PlayerObjective.IDLE,
+            new PlayerState(
+                Collections.emptySet(),
+                Collections.emptySet(),
+                new AbstractSet<>() {
+                    @Override
+                    public Iterator<Edge> iterator() {
+                        return Iterators.singletonIterator(null);
+                    }
+
+                    @Override
+                    public int size() {
+                        return 1;
+                    }
+                },
+                Collections.emptyList(),
+                null,
+                0,
+                Collections.emptyMap()));
+        call(() -> updateBuildRoadButtonStateMethod.invoke(playerActionsController), baseContext, result ->
+            "PlayerActionsController.updateBuildRoadButtonState threw an uncaught exception");
+        assertFalse(calledEnableBuildRoadButton.get(), baseContext, result ->
+            "PlayerActionsController.updateBuildRoadButtonState called builder.enableBuildRoadButton");
+        assertTrue(calledDisableBuildRoadButton.get(), baseContext, result ->
+            "PlayerActionsController.updateBuildRoadButtonState did not call builder.disableBuildRoadButton");
+
+        // correct objective, one buildable intersection
+        calledEnableBuildRoadButton.set(false);
+        calledDisableBuildRoadButton.set(false);
+        setup(updateBuildRoadButtonStateMethod.getName(),
+            calledEnableBuildRoadButton,
+            calledDisableBuildRoadButton,
+            PlayerObjective.REGULAR_TURN,
+            new PlayerState(
+                Collections.emptySet(),
+                Collections.emptySet(),
+                new AbstractSet<>() {
+                    @Override
+                    public Iterator<Edge> iterator() {
+                        return Iterators.singletonIterator(null);
+                    }
+
+                    @Override
+                    public int size() {
+                        return 1;
+                    }
+                },
+                Collections.emptyList(),
+                null,
+                0,
+                Collections.emptyMap()));
+        call(() -> updateBuildRoadButtonStateMethod.invoke(playerActionsController), baseContext, result ->
+            "PlayerActionsController.updateBuildRoadButtonState threw an uncaught exception");
+        assertTrue(calledEnableBuildRoadButton.get(), baseContext, result ->
+            "PlayerActionsController.updateBuildRoadButtonState did not call builder.enableBuildRoadButton");
+        assertFalse(calledDisableBuildRoadButton.get(), baseContext, result ->
+            "PlayerActionsController.updateBuildRoadButtonState called builder.disableBuildRoadButton");
+
+        // correct objective, one buildable intersection
+        calledEnableBuildRoadButton.set(false);
+        calledDisableBuildRoadButton.set(false);
+        setup(updateBuildRoadButtonStateMethod.getName(),
+            calledEnableBuildRoadButton,
+            calledDisableBuildRoadButton,
+            PlayerObjective.PLACE_ROAD,
+            new PlayerState(
+                Collections.emptySet(),
+                Collections.emptySet(),
+                new AbstractSet<>() {
+                    @Override
+                    public Iterator<Edge> iterator() {
+                        return Iterators.singletonIterator(null);
+                    }
+
+                    @Override
+                    public int size() {
+                        return 1;
+                    }
+                },
+                Collections.emptyList(),
+                null,
+                0,
+                Collections.emptyMap()));
+        call(() -> updateBuildRoadButtonStateMethod.invoke(playerActionsController), baseContext, result ->
+            "PlayerActionsController.updateBuildRoadButtonState threw an uncaught exception");
+        assertTrue(calledEnableBuildRoadButton.get(), baseContext, result ->
+            "PlayerActionsController.updateBuildRoadButtonState did not call builder.enableBuildRoadButton");
+        assertFalse(calledDisableBuildRoadButton.get(), baseContext, result ->
+            "PlayerActionsController.updateBuildRoadButtonState called builder.disableBuildRoadButton");
     }
 }
